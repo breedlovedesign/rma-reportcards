cad31b9753f3a2245f1f077e1bfab663e4e4372d
breedlovedesign <localadmin@Johns-MacBook-Air.local>
2013-09-18 22:25:21 +0700

    Created a throw-away database to experiment on.
    Changed the config to point at the new DB.

--- models/init.rb
+++ models/init.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 require 'mongoid'
 Mongoid.configure do |config|
-  config.sessions = {:default => {:hosts => ["localhost:27017"], :database => "rma"}}
+  config.sessions = {:default => {:hosts => ["localhost:27017"], :database => "rma-development"}}
 end
 SUBJECTS = [:language_arts, :math, :social_studies, :science, :art, :ict, :music, :thai, :pe, :work_study, :citizenship]
 require_relative 'user'
