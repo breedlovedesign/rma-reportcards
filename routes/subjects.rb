f585f52708027065003e0e64ea545a1dcdf2448e
breedlovedesign <localadmin@Johns-MacBook-Air.local>
2013-10-05 18:16:38 +0700

    Locked down subjects page.
    I had forgotten to save this file, it should have been in the last
    commit.

--- routes/subjects.rb
+++ routes/subjects.rb
@@ -1,27 +1,32 @@
 # encoding: utf-8
 get "/subjects" do
+	admin?
   @title = "Subjects"	
   @subjects = Subject.all
   haml :"subjects/index"
 end
 
-post "/subjects/new" do 
+post "/subjects/new" do
+	admin?
   p = params[:subject]
   Subject.where(name: p["name"], subject_id: p["subject_id"]).create 
   redirect to('/subjects')
 end
 
 delete '/subjects/delete/:id' do
+	admin?
   Subject.where(id: params[:id]).destroy
   redirect to('/subjects')
 end
 
 get "/subjects/edit/:mongo_id" do
+	admin?
   @subject = Subject.find_by(id: params[:mongo_id])
   haml :"subjects/edit"   
 end
 
-post "/subjects/update/:mongo_id" do 
+post "/subjects/update/:mongo_id" do
+	admin?
   form = params[:form_data_2]
   pants = Subject.find(params[:mongo_id])
   pants.update_attributes!(:subject_id => form["subject_id"]) unless form["subject_id"] == ""
