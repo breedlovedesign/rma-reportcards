836283981075037a05262184cc9a986d6c9d4056
John Breedlove <breedlovedesign@gmail.com>
2014-02-25 14:11:00 +0700

    created form for peer editing stage

--- routes/edits.rb
+++ routes/edits.rb
@@ -33,6 +33,8 @@ get "/edits/:teacher_id" do
 	language_arts_students = []
 	language_arts_outcome_sets.each do |set| 
 		language_arts_component = {}
+		language_arts_component[:skill_track] = set.skill_track
+		language_arts_component[:language_arts_level] = set.skill_track.language_arts_level
 		language_arts_component[:student_name] = Student.find("#{set.skill_track.student_id}").name
 		language_arts_component[:comment_object] = set.commentos 
 		language_arts_students << language_arts_component
@@ -42,29 +44,83 @@ get "/edits/:teacher_id" do
 	haml :"edits/teacher_subjects", :layout => :teacher_layout
 end
 
-# language_arts_teacher
-# math_teacher
-# social_studies_teacher
-# science_teacher
-# art_teacher
-# ict_teacher
-# music_teacher
-# thai_teacher
-# pe_teacher
-# work_study_teacher
-# citizenship_teacher
+post "/edit/submit/:subject_id/:student_id/:skill_track_id" do
+	form = params[:comment_form]
 
-# get "/edits/:teacher_id" do
-# 	@teacher  = Teacher.find_by(:id => params[:teacher_id])
-# 	@students = Student.all.map do |student|
-# 		if student.current_skill_track.has_teacher(@teacher.id.to_s)
-# 			student # build a list in @students of this teacher's students
-# 		end
-# 	end
-# 	@students.compact!
-# 	@subjects = @students.map do |student|
-# 		student.current_skill_track.which_subjects(@teacher.id.to_s)
-# 	end
-# 	@subjects = @subjects.flatten.uniq
-# 	haml :"edits/teacher_subjects", :layout => :teacher_layout
-# end
+	student = Student.find_by(:id => params[:student_id])
+	skill_track = student.skill_tracks.find_by(:id => params[:skill_track_id])
+
+unless form["language_arts_comment"].nil?
+	language_arts_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "language_arts"))
+	#language_arts_outcome_set.commentos.create!(:texto => form["language_arts_comment"])
+	raw_comment = form["language_arts_comment"]
+	processed_comment = raw_comment.paragraph_prep
+	doub = doublespacer(processed_comment)
+	doub.paragraph_reinsertion
+	language_arts_outcome_set.commentos.create!(:texto => doub)
+
+	language_arts_outcome_set.save
+end	
+
+unless form["math_comment"].nil?
+	math_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "math"))
+	math_outcome_set.commentos.create!(:texto => form["math_comment"])
+	math_outcome_set.save
+end	
+
+unless form["social_studies_comment"].nil?
+	social_studies_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "social_studies"))
+	social_studies_outcome_set.commentos.create!(:texto => form["social_studies_comment"])
+	social_studies_outcome_set.save
+end	
+
+unless form["science_comment"].nil?
+	science_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "science"))
+	science_outcome_set.commentos.create!(:texto => form["science_comment"])
+	science_outcome_set.save
+end	
+
+unless form["work_study_comment"].nil?
+	work_study_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "work_study"))
+	work_study_outcome_set.commentos.create!(:texto => form["work_study_comment"])
+	work_study_outcome_set.save
+end	
+
+unless form["citizenship_comment"].nil?
+	citizenship_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "citizenship"))
+	citizenship_outcome_set.commentos.create!(:texto => form["citizenship_comment"])
+	citizenship_outcome_set.save
+end	
+
+unless form["art_comment"].nil?
+	art_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "art"))
+	art_outcome_set.commentos.create!(:texto => form["art_comment"])
+	art_outcome_set.save
+end	
+
+unless form["ict_comment"].nil?
+	ict_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "ict"))
+	ict_outcome_set.commentos.create!(:texto => form["ict_comment"])
+	ict_outcome_set.save
+end	
+
+unless form["music_comment"].nil?
+	music_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "music"))
+	music_outcome_set.commentos.create!(:texto => form["music_comment"])
+	music_outcome_set.save
+end	
+
+unless form["pe_comment"].nil?
+	pe_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "pe"))
+	pe_outcome_set.commentos.create!(:texto => form["pe_comment"])
+	pe_outcome_set.save
+end	
+
+unless form["thai_comment"].nil?
+	thai_outcome_set = skill_track.outcome_sets.find_by(:subject_id => Subject.find_by(:subject_id => "thai"))
+	thai_outcome_set.commentos.create!(:texto => form["thai_comment"])
+	thai_outcome_set.save
+end		
+
+	redirect to("/commenting/#{params[:student_id]}\##{params[:subject_id]}")
+end
